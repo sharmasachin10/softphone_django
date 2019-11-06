bbef672008d5995f53c6c14e52c76d25cb5f01c2
Sachin Sharma <sharmasachin15590@gmail.com>
2019-11-06 15:00:35 +0530

    initial commit

---
+++ Softphone/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import plivo
+from .plivo_creds import * 
+
+##############################################FUNCTIONS##################################################
+#Home
+def index(request):
+    return render(request, "index.html", {})
+
+#Make Call
+def make_call(request):
+    print("index.html>>",request.POST,auth_id)
+    to_number = request.POST.get("call_number")
+    client = plivo.RestClient(auth_id=auth_id, auth_token=auth_token)
+    call_made = client.calls.create(
+        from_=calling_number,
+        to_=to_number,
+        answer_url=answer_url
+    )
+    return render(request, "call_progress.html", {})
+
+
+
+#Receive Call
+def receive_call():
+    # Generate a Speak XML with the details of the text to play on the call.
+    body = "Hello, you just received your first call"
+    r = plivoxml.Response()
+    r.addSpeak(body)
+
+    print (r.to_xml())
+    return Response(str(r), mimetype='text/xml')
