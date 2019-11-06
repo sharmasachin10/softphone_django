f4b624e33c4cc074c2f2f150de17e1b7ba06a6c1
Sachin Sharma <sharmasachin15590@gmail.com>
2019-11-06 15:09:51 +0530

    allowed hosts update

--- Softphone/plivo_creds.py
+++ Softphone/plivo_creds.py
@@ -1,4 +1,4 @@
 calling_number='+18323243680'
 auth_id='MANZE1ZTHJZJKWNZY4YW'
 auth_token='MDUwMDczNmQ4NTQ5ZjA0OWQ1YmQ3M2Y4OTI5NzAx'
-answer_url='http://127.0.0.1:8000/receive_call/'
+answer_url='http://206.189.57.134:3000/receive_call/'
